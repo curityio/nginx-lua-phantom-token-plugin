cc27383b97f867134679081f8e8dd08359c4bc01
gary <gary@WORK.local>
2026-08-19 19:49:06 +0200

    Made token_cache_seconds optional

--- plugin/schema.lua
+++ plugin/schema.lua
@@ -8,7 +8,7 @@ return {
                 { introspection_endpoint = { type = "string", required = true, match = "^https?://" } },
                 { client_id = { type = "string", required = true } },
                 { client_secret = { type = "string", required = true } },
-                { token_cache_seconds = { type = "number", required = true } },
+                { token_cache_seconds = { type = "number", required = false } },
                 { scope = { type = "string", required = false } },
                 { verify_ssl = { type = "boolean", required = false } },
                 { scheme = { type = "string", required = false, one_of = { "Bearer", "DPoP" } } }
